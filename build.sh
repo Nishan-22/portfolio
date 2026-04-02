be0ccabacdbe4494e434ab9235eb1c9d9b04945e
Nishan-22 <paudelnishan47@gmail.com>
2026-04-02 18:29:02 +0545

    yaml file

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # Build script for Render deployment
 
-# Exit on error
 set -o errexit
 
 # Install dependencies
@@ -12,3 +11,6 @@ python manage.py collectstatic --no-input --clear
 
 # Run database migrations
 python manage.py migrate
+
+# Create superuser (if not exists)
+python manage.py createsuperuser --noinput || true
